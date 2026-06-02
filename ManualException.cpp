308007d0351feb22a27ed54034ad72fbe31d1748
khansa-qonita <khansaqonita420@gmail.com>
2026-06-02 18:00:27 +0700

    menambahkan library dan membuat isi dari try untuk pengujian error

---
+++ ManualException.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY " << endl;
+        
+    }
